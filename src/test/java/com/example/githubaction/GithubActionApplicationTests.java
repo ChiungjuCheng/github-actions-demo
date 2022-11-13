a129980c8c8cd43984ce9173a618711e0a8734b3
casey <aj8c8c8@gmail.com>
2022-11-13 18:59:29 +0800

    github actions init

---
+++ src/test/java/com/example/githubaction/GithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubaction;
+
+import org.junit.jupiter.api.Test;
+
+
+class GithubActionApplicationTests {
+
+	@Test
+	void test_message() {
+		System.out.println("test github action success!");
+	}
+
+}
